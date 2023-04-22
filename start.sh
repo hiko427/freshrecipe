6d80764fe688a369c006e082ecf214de95a2756c
hiko427 <junyanweng24@gmail.com>
2023-04-23 00:38:32 +0900

    fix start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,11 @@
 set -e
 
 echo "run db migration"
+
+aws secretsmanager get-secret-value --secret-id recipe --query SecretString --output text | jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' > /app/app.env
+
 source /app/app.env
+
 /app/migrate -path /app/db/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
